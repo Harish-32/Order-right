845d9473dc62737814cae1e3e3736b32bef0528a
dharamoniharish@gmail.com <dharamoniharish@gmail.com>
2026-05-16 13:27:33 +0530

    change the file names

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import FarmerDashboard from "./pages/FarmerDashboard";
 import FarmerOrders from "./pages/FarmerOrders";
 import MyOrders from "./pages/MyOrders";
-import MyProducts from "./pages/Myproducts";
+import MyProducts from "./pages/MyProducts";
 import EditProduct from "./pages/EditProduct";
 function App() {
   return (
